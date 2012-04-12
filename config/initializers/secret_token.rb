f1af5a7b8991443ef323114437c215e6b151fb35
Vladislav Pilipenko <vladislav888@gmail.com>
2012-04-12 21:10:55 +0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JbxbijouxSpree::Application.config.secret_token = 'ad27b01cc4a30b555682df8819cb77140f42758d6fd031c1fc98e782b2edd59af84dae4b05a873ca7fed9ba7117317555939e021d5862637d68947477ca5b5f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JbxbijouxSpree::Application.config.session_store :cookie_store, key: '_jbxbijoux_spree_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JbxbijouxSpree::Application.config.session_store :active_record_store
